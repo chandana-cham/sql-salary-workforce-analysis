62f6dcc2f612d58e099b33b25b782dfb44bad9ee
CHANDANA SREE CHINNASETTY <chandanasreechinnasetty10@gmail.com>
2025-12-27 23:57:16 +0530

    Create ds_salaries table in schema.sql

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE ds_salaries (
+    work_year INT,
+    experience_level VARCHAR(5),
+    employment_type VARCHAR(5),
+    job_title VARCHAR(100),
+    salary INT,
+    salary_in_usd INT,
+    employee_residence VARCHAR(10),
+    remote_ratio INT,
+    company_location VARCHAR(10),
+    company_size VARCHAR(5)
+);
